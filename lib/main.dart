03f67d1dc4daec713b1527ad66a86d99878ad4c3
Kshitij Ukey <kshitijukey@Kshitijs-MacBook-Air.local>
2020-04-10 01:58:04 +0530

    Files Added

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'K', text: 'Life is Beautiful'),
+    Quote(author: 'L', text: 'Stay at Home, Stay Safe'),
+    Quote(author: 'M', text: 'India Fights Corona'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
+      ),
+    );
+  }
+}
